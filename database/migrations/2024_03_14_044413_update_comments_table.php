1c16896fa155989bd7a794f39f7e2833e3214076
kendzz <ngatu9x@gmail.com>
2024-03-14 11:47:26 +0700

    update migrations

---
+++ database/migrations/2024_03_14_044413_update_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\CommentType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->enum("type" ,[CommentType::Project, CommentType::Task]);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn("type");
+
+        });
+    }
+};
